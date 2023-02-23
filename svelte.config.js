8db5601b697d6e13ca29df3ee085bed2709778d6
Patric Pfoertner <patricpfoertner@Patrics-Mac-mini.local>
2023-02-23 14:01:01 +0200

    hi

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static'
 import { mdsvex } from 'mdsvex'
-import preprocess from 'svelte-preprocess'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeSlug from 'rehype-slug'
 
@@ -23,7 +22,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter({ out: 'build' }),
+		adapter: adapter(),
     prerender: {
       entries: [
         '*',
